bc82a8876741e869c4975f95a367421ea9e5def7
terri_gz <hongxuan0377@qq.com>
2017-12-28 17:54:46 +0800

    添加不同图形色彩展示分类

--- ch02/kNN.py
+++ ch02/kNN.py
@@ -57,6 +57,25 @@ reload(kNN)
 datingDataMat,datingLabels=kNN.file2matrix('datingTestSet2.txt')
 datingDataMat
 datingLabels[0:20]
+
+#### ex A:
+import matplotlib
+import matplotlib.pyplot as plt
+fig = plt.figure()
+ax= fig.add_subplot(111)
+ax.scatter(datingDataMat[:,1],datingDataMat[:,2])
+plt.show()
+
+## ex B:
+import matplotlib
+import matplotlib.pyplot as plt
+from numpy import *
+fig = plt.figure()
+ax= fig.add_subplot(111)
+ax.scatter(datingDataMat[:,1],datingDataMat[:,2],15.0*array(datingLabels),15
+.0*array(datingLabels))
+plt.show()
+
 '''
 
 
