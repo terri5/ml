0222cc60d9a0effef76ef546a3a30345ccadcd4b
terri_gz <hongxuan0377@qq.com>
2017-12-28 17:35:00 +0800

    添加对旅行数据的预测

--- ch02/kNN.py
+++ ch02/kNN.py
@@ -34,3 +34,29 @@ group
 labels
 group,labels=kNN.createDataSet();
 '''
+
+
+def file2matrix(filename):
+     fr = open(filename)
+     arrayOLines = fr.readlines()
+     numberOfLines = len(arrayOLines)
+     returnMat = zeros((numberOfLines,3))
+     classLabelVector = []
+     index = 0
+     for line in arrayOLines:
+         line=line.strip()
+         listFromLine = line.split('\t')
+         returnMat[index:] = listFromLine[0:3]
+         classLabelVector.append(int(listFromLine[-1]))
+         index += 1
+     return returnMat,classLabelVector
+
+'''
+CMD:
+reload(kNN)
+datingDataMat,datingLabels=kNN.file2matrix('datingTestSet2.txt')
+datingDataMat
+datingLabels[0:20]
+'''
+
+
