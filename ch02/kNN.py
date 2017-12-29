6c9038ea742741770a55a0500d90f50d35b31b62
terri_gz <hongxuan0377@qq.com>
2017-12-29 11:35:18 +0800

    添加归一化处理

--- ch02/kNN.py
+++ ch02/kNN.py
@@ -77,5 +77,14 @@ ax.scatter(datingDataMat[:,1],datingDataMat[:,2],15.0*array(datingLabels),15
 plt.show()
 
 '''
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals-minVals
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - tile(minVals,(m,1))
+    normDataSet = normDataSet/tile(ranges,(m,1))
+    return normDataSet,ranges,minVals
 
 
