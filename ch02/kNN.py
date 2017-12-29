93aa685c2d285a6aae4f5324a847e5902d790347
terri_gz <hongxuan0377@qq.com>
2017-12-29 14:59:40 +0800

    加入测试验证功能

--- ch02/kNN.py
+++ ch02/kNN.py
@@ -88,3 +88,21 @@ def autoNorm(dataSet):
     return normDataSet,ranges,minVals
 
 
+def datingClassTest():
+    hoRatio  = 0.10
+    datingDataMat,datingLabels= file2matrix('datingTestSet2.txt')
+    normMat,ranges,minVals=autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m*hoRatio)
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i,:],normMat[numTestVecs:m,:],
+                                     datingLabels[numTestVecs:m],3)
+        print 'the classsifierResult came back with: %d,the real answer is: %d '\
+                %(classifierResult,datingLabels[i])
+        if(classifierResult != datingLabels[i]) : errorCount +=1.0
+    print "the total error rate is: %f "%(errorCount/float(numTestVecs))
+
+
+
+
